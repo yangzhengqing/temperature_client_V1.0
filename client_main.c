09148852284ece2b9c1e6e0ab8fc885311893728
ubunt1404 <2427779305@qq.com>
2019-10-25 23:38:52 +0800

    achieve time 3s

--- client_main.c
+++ client_main.c
@@ -21,180 +21,199 @@
 
 void sig_quit(int);
 
+void sig_timer();
+
 void print_usage(char *order)
 {
-        printf("%s usages:\n",order);
-        printf("-I(IP):server IP\n");
-        printf("-p(port):server port\n");
-        printf("-h(help):help information\n");
+	printf("%s usages:\n",order);
+	printf("-I(IP):server IP\n");
+	printf("-p(port):server port\n");
+	printf("-h(help):help information\n");
 
-        exit(0);
+	exit(0);
 }
 
 int get_stop = 0;
+int sampleOver_statu = 0;
+float temp;
+char chip[20];
 
 int main(int argc,char **argv)
 {
-        char                   *serv_IP = NULL;
-        int                    serv_port = 0;
-        int                    opt;
-        struct sockaddr_in     serv_addr;
-        char                   buf[100];
-        char                   buf1[100];
-        char                   chip[20];
-        int                    sockfd;
-        int                    rv = -1;
-        int                    on = 1;
-        float                  temp;
-        int                    line;
-        struct timespec        tout;
-        struct tm              *tmp;
-        struct hostent         *hptr;
-        struct option long_options[]=
-        {
-                
-                {"IP",1,NULL,'i'},
-                {"port",1,NULL,'p'},
-                {"help",0,NULL,'h'},
-                {0,0,0,0}
-        
-        };
+	char                   *serv_IP = NULL;
+	int                    serv_port = 0;
+	int                    opt;
+	struct sockaddr_in     serv_addr;
+	char                   buf[100];
+	char                   buf1[100];
+	int                    sockfd;
+	int                    rv = -1;
+	int                    on = 1;
+	int                    line;
+	struct timespec        tout;
+	struct tm              *tmp;
+	struct hostent         *hptr;
+	struct option long_options[]=
+	{
 
-        hptr = gethostbyname(argv[1]);
-        if(hptr != NULL)
-        {
-                printf("domian name visit.\n");
-                           
-                serv_IP = hptr->h_addr_list[1];
+		{"IP",1,NULL,'i'},
+		{"port",1,NULL,'p'},
+		{"help",0,NULL,'h'},
+		{0,0,0,0}
 
-                printf("IP address2:%s\n",inet_ntoa(*(struct in_addr *)serv_IP));
+	};
 
-        }
-        else if(hptr == NULL)
-        {
-                while((opt=getopt_long(argc,argv,"i:p:h",long_options,NULL)) > 0)
-                {
+	hptr = gethostbyname(argv[1]);
+	if(hptr != NULL)
+	{
+		printf("domian name visit.\n");
 
-                        switch(opt)
-                        {
+		serv_IP = hptr->h_addr_list[1];
+
+		printf("IP address2:%s\n",inet_ntoa(*(struct in_addr *)serv_IP));
+
+	}
+	else if(hptr == NULL)
+	{
+		while((opt=getopt_long(argc,argv,"i:p:h",long_options,NULL)) > 0)
+		{
+
+			switch(opt)
+			{
 
 
-                         case 'i':
-                                serv_IP = optarg;
-                                break;
-                         case 'p':
-                                serv_port = atoi(optarg);
-                                break;
-                         case 'h':
-                                print_usage(argv[0]);
-                                break;
-                         default:
-                                break;
+				case 'i':
+					serv_IP = optarg;
+					break;
+				case 'p':
+					serv_port = atoi(optarg);
+					break;
+				case 'h':
+					print_usage(argv[0]);
+					break;
+				default:
+					break;
 
-                       }
-      
-                }
+			}
 
-                if((!serv_IP)|(!serv_port))
-                {
-                         print_usage(argv[0]);
-                }
-       }
+		}
 
-       
-        if(signal(SIGQUIT,sig_quit) < 0)
-        {
-                printf("signal SIGKILL failed:%s\n",strerror(errno));
-        }
-
-        if((sockfd = socket(AF_INET,SOCK_STREAM,0)) < 0)
-        {
-                printf("socket failed: %s\n",strerror(errno));
-          //      mylog(__FUNCTION__,__LINE__,ERROR,"socket failed:%s\n",strerror(errno));
-                return -1;
-        }
-
-        setsockopt(sockfd,SOL_SOCKET,SO_REUSEADDR,&on,sizeof(on));
-
-        memset(&serv_addr,0,sizeof(serv_addr));
-        serv_addr.sin_family = AF_INET;
-        serv_addr.sin_port = htons(serv_port);
-        inet_aton(serv_IP,&serv_addr.sin_addr);
-         
-        memset(buf,0,sizeof(buf));
-
-        printf("client is connecting serve...\n");
-
-        if(connect(sockfd,(struct sockaddr *)&serv_addr,sizeof(serv_addr)) < 0)
-        {
-                printf("connect failed: %s\n",strerror(errno));
-          //      mylog(__FUNCTION__,__LINE__,ERROR,"connect failed:%s\n",strerror(errno));
-        }
-        else
-        {
-
-                printf("Connect successfully...\n");
-        }
+		if((!serv_IP)|(!serv_port))
+		{
+			print_usage(argv[0]);
+		}
+	}
 
 
-        while(!get_stop)
-        {
-                 
-                DS_get_tem(&temp,chip);
-                tmp = localtime(&tout.tv_sec);
-                strftime(buf1,sizeof(buf1),"%r",tmp);
+	if(signal(SIGQUIT,sig_quit) < 0)
+	{
+		printf("signal SIGKILL failed:%s\n",strerror(errno));
+	}
 
-                sprintf(buf,"%s %.3f℃ %s",chip,temp,buf1);
+	if(signal(SIGALRM,sig_timer) < 0)
+	{
+		printf("signal SIGALARM failed:%s\n",strerror(errno));
+	}
 
-                if(write(sockfd,buf,strlen(buf)) <0 )
-                {
+	if((sockfd = socket(AF_INET,SOCK_STREAM,0)) < 0)
+	{
+		printf("socket failed: %s\n",strerror(errno));
+		//      mylog(__FUNCTION__,__LINE__,ERROR,"socket failed:%s\n",strerror(errno));
+		return -1;
+	}
 
-                        printf("write to serve unsuccessfully: %s\n",strerror(errno));
-            //            mylog(__FUNCTION__,__LINE__,ERROR,"write to serve failed:%s\n",strerror(errno));
-                        goto cleanup;
-                }
+	setsockopt(sockfd,SOL_SOCKET,SO_REUSEADDR,&on,sizeof(on));
 
-		printf("temprature:%s\n",buf);
+	memset(&serv_addr,0,sizeof(serv_addr));
+	serv_addr.sin_family = AF_INET;
+	serv_addr.sin_port = htons(serv_port);
+	inet_aton(serv_IP,&serv_addr.sin_addr);
 
-                memset(buf,0,sizeof(buf));
-            
+	memset(buf,0,sizeof(buf));
 
-                if((rv = read(sockfd,buf,sizeof(buf))) < 0)
-                {   
-                        printf("read from serve unsuccessfully: %s\n",strerror(errno));
-              //          mylog(__FUNCTION__,__LINE__,ERROR,"read from serve failed:%s\n",strerror(errno));
-                        goto cleanup;
-                }
+	printf("client is connecting serve...\n");
 
-                if(rv == 0)
-                { 
-                        printf("disconnect serve\n");
-                //        mylog(__FUNCTION__,__LINE__,ERROR,"disconnect failed:%s\n",strerror(errno));
-                        goto cleanup;
-                }
+	if(connect(sockfd,(struct sockaddr *)&serv_addr,sizeof(serv_addr)) < 0)
+	{
+		printf("connect failed: %s\n",strerror(errno));
+		//      mylog(__FUNCTION__,__LINE__,ERROR,"connect failed:%s\n",strerror(errno));
+	}
+	else
+	{
 
-                printf(" %s\n",buf);
+		printf("Connect successfully...\n");
+	}
+	
+	alarm(3);//定时开始，超时采样，采样结束上传服务器
+	while(!get_stop)
+	{
+		// DS_get_tem(&temp,chip);
+		if(sampleOver_statu == 1)
+		{
+			tmp = localtime(&tout.tv_sec);
+			strftime(buf1,sizeof(buf1),"%r",tmp);
 
-                memset(buf,0,sizeof(buf));
+			sprintf(buf,"%s %.3f℃ %s",chip,temp,buf1);
 
-                sleep(10);          
-           }
+			if(send(sockfd,buf,strlen(buf),0) <0 )
+			{
 
-        close(sockfd);
+				printf("write to serve unsuccessfully: %s\n",strerror(errno));
+				//            mylog(__FUNCTION__,__LINE__,ERROR,"write to serve failed:%s\n",strerror(errno));
+				goto cleanup;
+			}
+
+			printf("temprature:%s\n",buf);
+
+			memset(buf,0,sizeof(buf));
+
+
+			if((rv = recv(sockfd,buf,sizeof(buf),0)) < 0)
+			{   
+				printf("read from serve unsuccessfully: %s\n",strerror(errno));
+				//          mylog(__FUNCTION__,__LINE__,ERROR,"read from serve failed:%s\n",strerror(errno));
+				goto cleanup;
+			}
+
+			if(rv == 0)
+			{ 
+				printf("disconnect serve\n");
+				//        mylog(__FUNCTION__,__LINE__,ERROR,"disconnect failed:%s\n",strerror(errno));
+				goto cleanup;
+			}
+
+			printf(" %s\n",buf);
+
+			memset(buf,0,sizeof(buf));
+
+			sampleOver_statu = 0;
+
+			// sleep(10);
+		}          
+	}
+
+	close(sockfd);
 
 cleanup:
-        close(sockfd);
-        return 0;
+	close(sockfd);
+	return 0;
 
 }
 
 
 void sig_quit(int signo)
 {
-    get_stop = 1;
+	printf("programer will be stop...\n");
+	get_stop = 1;
 }
 
-
+void sig_timer(int signo)
+{
+	printf("pi start sample...\n");
+	DS_get_tem(&temp,chip);
+	sampleOver_statu = 1;
+	alarm(3);
+}
 
 
 
